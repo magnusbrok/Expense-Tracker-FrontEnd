e4529ab4669ea58a48a17257ba15eaade20437d2
Anekel <43749071+Anekel@users.noreply.github.com>
2020-03-23 17:36:02 +0100

    Added pie chart template

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvasjs.min';
+// var CanvasJS = require('./canvasjs.min');
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  ngOnInit() {
+    const chart = new CanvasJS.Chart('chartContainer', {
+      theme: 'dark1',
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: 'Expenses This Month'
+      },
+      data: [{
+        type: 'pie',
+        showInLegend: true,
+        toolTipContent: '<b>{name}</b>: ${y} (#percent%)',
+        indexLabel: '{name} - #percent%',
+        dataPoints: [
+          { y: 450, name: 'Food' },
+          { y: 120, name: 'Insurance' },
+          { y: 300, name: 'Traveling' },
+          { y: 800, name: 'Housing' },
+          { y: 150, name: 'Education' },
+          { y: 150, name: 'Shopping'},
+          { y: 250, name: 'Others' }
+        ]
+      }]
+    });
 
-  constructor() { }
-
-  ngOnInit(): void {
+    chart.render();
   }
 
 }
