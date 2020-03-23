d1dacecca0320fc7e55bf1fd749ea7be6762c9bb
Anekel <43749071+Anekel@users.noreply.github.com>
2020-03-23 14:24:50 +0100

    added expenses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { BudgetComponent } from './budget/budget.component';
+import { ExpensesComponent } from './expenses/expenses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WelcomePageComponent,
-    BudgetComponent
+    BudgetComponent,
+    ExpensesComponent
   ],
   imports: [
     BrowserModule
