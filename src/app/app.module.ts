c2538c1eff090226a71b29bc65d4e00af1389523
Anekel <43749071+Anekel@users.noreply.github.com>
2020-03-23 14:23:22 +0100

    added welcome page component and removed test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule
