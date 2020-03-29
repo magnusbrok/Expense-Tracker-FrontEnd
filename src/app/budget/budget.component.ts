a550169df66724cfd5b8af05857d056bc7380b7f
Helle <helle1926@hotmail.com>
2020-03-29 12:48:56 +0200

    Better outline for design

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import * as CanvasJS from './canvasjs.min';
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -8,11 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor() {}
 
 
+  ngOnInit() {
+      const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+          text: 'Oversigt over dine seneste udgifter '
+        },
+        data: [{
+          type: 'pie',
+          showInLegend: true,
+          toolTipContent: '<b>{name}</b>: ${y} (#percent%)',
+          indexLabel: '{name} - #percent%',
+          dataPoints: [
+            { y: 450, name: 'Apple' },
+            { y: 120, name: 'Mango' },
+            { y: 300, name: 'Orange' },
+            { y: 800, name: 'Banana' },
+            { y: 150, name: 'Pineapple' }
+          ]
+        }]
+      });
 
+      chart.render();
+    }
 }
