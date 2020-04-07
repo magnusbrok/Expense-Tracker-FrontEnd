2734e7490d1dcb0e38dba597dae130b7afc184df
Siff <siff.ravn@gmail.com>
2020-04-07 17:31:18 +0200

    feat (profile): Populate user data

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthenticationService} from "../authentication/authentication.service";
+import {Subscription} from "rxjs";
+import {User} from "../authentication/user.model";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
+
+  // subscription = new Subscription();
+  user : User;
+
 
   ngOnInit(): void {
+    // this.subscription = this.authService.userChanged
+    //   .subscribe(
+    //     (user: User) => { this.user = user}
+    //   );
+
+    this.user = this.authService.getUser();
+  }
+
+  ngOnDestroy(): void {
+    // this.subscription.unsubscribe();
   }
 
 }
