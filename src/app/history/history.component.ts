9012ba1e6abd50d28e9ef92bc3e28385429d34ac
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2020-04-14 16:36:34 +0200

    Update history.component.ts

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -12,7 +12,15 @@ export class HistoryComponent implements OnInit {
     new History('January', 35000, 36000, -1000),
     new History('February', 34000, 32000, 2000),
     new History('March', 41000, 42000, -1000),
-    new History('April', 39000, 41000, -2000)
+    new History('April', 39000, 41000, -2000),
+    new History('May', 34000, 32000, 2000),
+    new History('June', 41000, 42000, -1000),
+    new History('July', 35000, 36000, -1000),
+    new History('August', 34000, 32000, 2000),
+    new History('September', 41000, 42000, -1000),
+    new History('October', 41000, 42000, -1000),
+    new History('November', 41000, 42000, -1000),
+    new History('December', 41000, 42000, -1000)
   ];
 
   constructor() {
