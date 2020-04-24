34a72a210755bb9c2cfb3bc09ea0fbaccb1156bf
Siff <siff.ravn@gmail.com>
2020-04-24 10:59:21 +0200

    Feat (Back end communication) Implement basic CRUD methods for Budget

--- src/app/back-end.service.ts
+++ src/app/back-end.service.ts
@@ -1,10 +1,47 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {BudgetModel} from "./budget/budget.model";
+import {BudgetListService} from "./shared/budget-list.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackEndService {
 
-  constructor(private http: HttpClient) { }
+  domain : string= `http://localhost:3344`;
+  path : string = `budget`;
+
+  constructor(
+    private http: HttpClient,
+    private budgetListService : BudgetListService
+  ) { }
+
+  getBudgetList(){
+    let url = `${ this.domain }/${this.path}`;
+    return this.http.get<BudgetModel>(url);
+  }
+
+  getBudget(year : number, month : number){
+    let url = `${ this.domain }/${this.path}?year=${ year }&month=${ month }`;
+
+    return this.http.get<BudgetModel>(url);
+  }
+
+  createBudget(id : number){
+    let budget = this.budgetListService.getBudget(id);
+    let url = `${this.domain}//${this.path}`;
+
+    this.http.post(
+      url,
+      budget
+    )
+  }
+
+  updateBudget(id : number){
+
+  }
+
+  deleteBudget(id: number){
+
+  }
 }
