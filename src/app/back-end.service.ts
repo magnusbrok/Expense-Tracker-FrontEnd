c79a7cd3e7727e8ca8a6716fce811c0515d3a614
magnusbrok <magnus.brok@db-data.dk>
2020-04-24 12:07:08 +0200

    Update back-end.service.ts

--- src/app/back-end.service.ts
+++ src/app/back-end.service.ts
@@ -1,52 +1,53 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {BudgetModel} from "./budget/budget.model";
-import {BudgetListService} from "./shared/budget-list.service";
-import {AuthenticationService} from "./authentication/authentication.service";
+import {HttpClient} from '@angular/common/http';
+
+import {BudgetPostListService} from './shared/budget-post-list.service';
+import {AuthenticationService} from './authentication/authentication.service';
+import {Budget} from './budget/budget.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackEndService {
 
-  domain : string= `http://localhost:3344`;
-  path : string = `budget`;
+  domain = `http://localhost:3344`;
+  path = `budget`;
 
   constructor(
     private http: HttpClient,
-    private authService : AuthenticationService,
-    private budgetListService : BudgetListService
+    private authService: AuthenticationService,
+    private budgetPostListService: BudgetPostListService
   ) { }
 
-  getBudgetList(){
-    let url = `${ this.domain }/${this.path}`;
-    return this.http.get<BudgetModel>(url);
+  getBudgetList() {
+    const url = `${ this.domain }/${this.path}`;
+    return this.http.get<Budget>(url);
   }
 
-  getBudget(year : number, month : number){
+  getBudget(year: number, month: number) {
 
     const username = this.authService.getUser().username;
-    let url = `${ this.domain }/${username}/${this.path}?year=${ year }&month=${ month }`;
+    const url = `${ this.domain }/${username}/${this.path}?year=${ year }&month=${ month }`;
 
-    return this.http.get<BudgetModel>(url);
+    return this.http.get<Budget>(url);
   }
 
-  createBudget(id : number){
+  createBudget(id: number) {
     const username = this.authService.getUser().username;
-    let budget = this.budgetListService.getBudget(id);
-    let url = `${ this.domain }/${username}/${this.path}`;
+    const budget = this.budgetPostListService.getBudget(id);
+    const url = `${ this.domain }/${username}/${this.path}`;
 
     this.http.post(
       url,
       budget
-    )
+    );
   }
 
-  updateBudget(id : number){
+  updateBudget(id: number) {
 
   }
 
-  deleteBudget(id: number){
+  deleteBudget(id: number) {
 
   }
 }
