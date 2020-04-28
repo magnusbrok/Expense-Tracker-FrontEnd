533328359ab813c8b4584e7ea2aa52dfc05be2a2
Siff <siff.ravn@gmail.com>
2020-04-28 08:40:01 +0200

    Fix (Backend communication) updateBudget corrected

--- src/app/budget-post-edit/budget-post-edit.component.ts
+++ src/app/budget-post-edit/budget-post-edit.component.ts
@@ -58,7 +58,7 @@ export class BudgetPostEditComponent implements OnInit, OnDestroy{
   }
 
   onSaveBudget() {
-    this.backEndService.createBudget(this.budgetService.getCurrentBudget().month);
+    this.backEndService.updateBudget(this.budgetService.getCurrentBudget().year, this.budgetService.getCurrentBudget().month);
   }
 
   ngOnDestroy(): void {
