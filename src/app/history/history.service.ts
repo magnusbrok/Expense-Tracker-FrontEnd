81887f43e0e732c4b6a26acaa691aea95ac375af
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2020-05-02 14:21:34 +0200

    History på rette vej

--- src/app/history/history.service.ts
+++ src/app/history/history.service.ts
@@ -10,17 +10,7 @@ export class HistoryService {
 
   private histories: History[] = [
     new History('January', 35000, 36000, -1000),
-    new History('February', 34000, 32000, 2000),
-    new History('March', 41000, 42000, -1000),
-    new History('April', 39000, 41000, -2000),
-    new History('May', 34000, 32000, 2000),
-    new History('June', 41000, 42000, -1000),
-    new History('July', 35000, 36000, -1000),
-    new History('August', 34000, 32000, 2000),
-    new History('September', 41000, 42000, -1000),
-    new History('October', 41000, 42000, -1000),
-    new History('November', 41000, 42000, -1000),
-    new History('December', 41000, 42000, -1000)
+    new History('February', 34000, 32000, 2000)
   ];
 
   getHistories() {
