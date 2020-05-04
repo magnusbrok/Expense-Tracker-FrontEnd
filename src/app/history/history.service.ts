fb72b1d83d21282464f3804092c53abcf2b6a34b
Kjaerr97 <43874990+Kjaerr97@users.noreply.github.com>
2020-05-04 09:39:18 +0200

    History page layout done

--- src/app/history/history.service.ts
+++ src/app/history/history.service.ts
@@ -1,22 +1,25 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
 import { History} from './history.model';
 import {Expense} from '../expenses/expense.model';
 import {Budget} from '../budget/budget.model';
-
 
 @Injectable()
 export class HistoryService {
   historySelected = new Subject<number>();
   isHistorySelected = new Subject<boolean>();
 
+  expenses: Expense[] = [
+    new Expense(3000, 'Husleje', new Date(), '' ),
+    new Expense(100, 'Rejsekort', new Date(), '' ),
+    new Expense(500, 'Warzone', new Date(), '' )
+  ];
 
-  expenses: Expense[] = [new Expense(3000, 'test', new Date(), '' ),
-    new Expense(3000, 'test', new Date(), '' ), new Expense(3000, 'test', new Date(), '' )];
-
-
-  expenses2: Expense[] = [new Expense(30000, 'tewadwadst', new Date(), '' ),
-    new Expense(30, 'tdwdaawdt', new Date(), '' ), new Expense(3000, 'test', new Date(), '' )];
+  expenses2: Expense[] = [
+    new Expense(30000, 'Rejse', new Date(), '' ),
+    new Expense(30, 'Kebab', new Date(), '' ),
+    new Expense(1500, 'Shopping', new Date(), '' )
+  ];
 
   private histories: History[] = [
     new History(new Budget(2020, 1), this.expenses),
@@ -34,7 +37,4 @@ export class HistoryService {
   setHistoryList(historyList: History[]) {
     this.histories = historyList;
   }
-
-
-
 }
